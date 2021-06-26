d30624c1cdd49145f48285463407aabb9e6ace15
Adivishnu1006 <42071765+Adivishnu1006@users.noreply.github.com>
2021-06-26 20:48:30 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+fetch("https://type.fit/api/quotes")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+    let Rquote= Math.floor(Math.random()*1000);
+    document.getElementById("quote").innerHTML=data[Rquote].text
+    document.getElementById("author").innerHTML=data[Rquote].author
+
+   
+
+  });
+
+  function reloadPage(){
+    location.reload();
+}
+generateQuote();
